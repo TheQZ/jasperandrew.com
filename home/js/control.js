c75890b5823c040ec20a0b73d0c51b82226e26cb
jasperandrew <jasper@jasperandrew.com>
2016-01-07 13:53:27 -0800

    Streamlined control js.

--- home/js/control.js
+++ home/js/control.js
@@ -1,95 +1,40 @@
-var on = 0;
-
-onload = init;
+var b_who = document.getElementById("who");
+var b_design = document.getElementById("design");
+var b_code = document.getElementById("code");
+var t_title = document.getElementById("name");
+var t_face = document.getElementById("face");
 
 function init() {
-	var b_who = document.getElementById("who");
-	var b_design = document.getElementById("design");
-	var b_code = document.getElementById("code");
-	var t_title = document.getElementById("title");
+	b_who.classList.add("trans");
+	b_design.classList.add("trans");
+	b_code.classList.add("trans");
+	t_title.classList.add("trans");
+	t_face.classList.add("trans");
 
-	b_who.classList.add("t");
-	b_design.classList.add("t");
-	b_code.classList.add("t");
-	t_title.classList.add("t");
-	
 	b_who.onclick = who;
 	b_design.onclick = design;
 	b_code.onclick = code;
-	
 }
 
 function who() {
-	var b_who = document.getElementById("who");
-	if(b_who.classList.contains("off")){
-		return;
-	}
-	
-	var b_design = document.getElementById("design");
-	var b_code = document.getElementById("code");
-	var t_title = document.getElementById("title");
-	
-	if(!on){
-		on++;
-		toggleClass([b_who], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_design, b_code, t_title], "off");
-	}else{
-		on--;
-		toggleClass([b_who, t_title], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_design, b_code, t_title], "off");
-		window.setTimeout(function(){toggleClass([t_title], "on");}, 300);
-	}
+	if(b_who.classList.contains("off")) return;
+	toggleClass([b_who], "on");
+	toggleClass([b_design, b_code, t_title], "off");
+	toggleClass([t_face], "img-off");
 }
 
 function design() {
-	var b_design = document.getElementById("design");
-	if(b_design.classList.contains("off")){
-		return;
-	}
-	
-	var b_who = document.getElementById("who");
-	var b_code = document.getElementById("code");
-	var t_title = document.getElementById("title");
-	
-	if(!on){
-		on++;
-		toggleClass([b_design], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_who, b_code, t_title], "off");
-	}else{
-		on--;
-		toggleClass([b_design, t_title], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_who, b_code, t_title], "off");
-		window.setTimeout(function(){toggleClass([t_title], "on");}, 300);
-	}
+	if(b_design.classList.contains("off")) return;
+	toggleClass([b_design], "on");
+	toggleClass([b_who, b_code, t_title], "off");
+	toggleClass([t_face], "img-off");
 }
 
 function code() {
-	var b_code = document.getElementById("code");
-	if(b_code.classList.contains("off")){
-		return;
-	}
-
-	var b_who = document.getElementById("who");
-	var b_design = document.getElementById("design");
-	var t_title = document.getElementById("title");
-	
-	
-	if(!on){
-		on++;
-		toggleClass([b_code], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_who, b_design, t_title], "off");
-	}else{
-		on--;
-		toggleClass([b_code, t_title], "on");
-		toggleClass([b_who, b_design, b_code], "glo");
-		toggleClass([b_who, b_design, t_title], "off");
-		window.setTimeout(function(){toggleClass([t_title], "on");}, 300);
-	}
+	if(b_code.classList.contains("off")) return;
+	toggleClass([b_code], "on");
+	toggleClass([b_who, b_design, t_title], "off");
+	toggleClass([t_face], "img-off");
 }
 
 function toggleClass(els, name) {
@@ -97,3 +42,5 @@ function toggleClass(els, name) {
 		els[i].classList.toggle(name);
 	}
 }
+
+init();
